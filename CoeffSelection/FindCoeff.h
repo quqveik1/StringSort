0367a6ffcd495b6e23d3d428540132e71b6873ce
Alex Bogomol <quqveik.1@gmail.com>
2023-04-03 00:28:46 +0300

    CoeffSelection delete

--- CoeffSelection/FindCoeff.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "..\..\ALGORITHMICPROJECT\Q_CoordinatSystem.h"
-
-void findBestCoeff(coordinatSys& sys, double* k, double* b, Vector& kBound, Vector& bBound, Vector& xBound, double(*fnc)(double k, double b, double x), double (*originalFnc)(double x));
-double calcQuadratic(double k, double b, double x, double(*fnc)(double k, double b, double x), double (*originalFnc)(double x));
-void drawBestCoeff(coordinatSys& sys, double k, double b, Vector& xBound, double(*fnc)(double k, double b, double x), COLORREF color);
-void findAndDrawBestCoeff(coordinatSys& sys, coordinatSys& deltaSys, double(*fnc)(double k, double b, double x), double (*originalFnc)(double x), double* findedK, double* findedB, Vector& kBound, Vector& bBound, Vector& xBound, COLORREF color);
-COLORREF getQuadraticDeltaColor(double quadraticDelta);
