f19a8d18b8ff4db656c42ea1144f9c9c116de409
Alex Bogomol <quqveik.1@gmail.com>
2023-04-06 19:37:22 +0300

    quicksort nlogn gradient descent

--- MainCoorLinLayout.cpp
+++ MainCoorLinLayout.cpp
@@ -10,12 +10,15 @@ MainCoorLinLayout::MainCoorLinLayout(AbstractAppData* _app) :
 {
     setMatchParent(true);
     addWindow(topWnd);
-    topWnd.setCCells({ (double)maxArrLen, maxArrLen * log2(maxArrLen) });
+    topWnd.setCCells({ (double)maxArrLen, maxArrLen * log(maxArrLen) });
     topWnd.setMatchParentX(true);
     addWindow(bottomWnd);
-    bottomWnd.setCCells({ (double)maxArrLen, maxArrLen * log2(maxArrLen) });
+    bottomWnd.setCCells({ (double)maxArrLen, maxArrLen * log(maxArrLen) });
     bottomWnd.setMatchParentX(true);
     activeTimer = app->setTimer(updateFrequency);
+
+    topWnd.addLay();
+    topWnd.addLay();
 
     thread sortThread(&MainCoorLinLayout::startComputations, this);
     sortThread.detach();
@@ -30,14 +33,76 @@ void MainCoorLinLayout::startComputations()
         if (!app->getAppCondition()) break;
         randomArrFill(arr, _len);
         testIntQuickSort(arr, _len);
-        topWnd.addPoint({ (double)_len, (double)getCCmps() }, false);
-        bottomWnd.addPoint({ (double)_len, (double)getCExchanges() }, false);
+        double logres = _len * log(_len);
+
+        topWnd.addPoint({ (double)_len, (double)getCCmps() }, sortDataColor, 0, sortDataIndex, false);
+        topWnd.addPoint({ (double)_len, logres }, logColor, 0, logIndex, false);
+        bottomWnd.addPoint({ (double)_len, (double)getCExchanges() }, sortDataColor, 0, sortDataIndex, false);
+        bottomWnd.addPoint({ (double)_len, logres }, logColor, 0, logIndex, false);
     }
 
     invalidateButton();
     delete[] arr;
 
     cout << clock() - start << "ms заняли вычесления\n";
+    gradientDescent();
+    drawGradientOdds();
+
+    isActiveComputingPart = false;
+}
+
+void MainCoorLinLayout::gradientDescent()
+{
+   
+    Vector learning_rate = { 1e-10, 1e-10};
+    Vector gradientDelta = { 0.1, 0.1 };
+    int iterations = 1000;
+    for (int i = 0; i < iterations; i++)
+    {
+        if (!app->getAppCondition()) break;
+        double currQuadraticDelta = countQuadraticDelta(gradientOdds.x, gradientOdds.y);
+        double dxQuadraticDelta = countQuadraticDelta(gradientOdds.x + gradientDelta.x, gradientOdds.y);
+        double dyQuadraticDelta = countQuadraticDelta(gradientOdds.x, gradientOdds.y + gradientDelta.y);
+
+        double xDerivative = (dxQuadraticDelta - currQuadraticDelta) / gradientDelta.x;
+        double yDerivative = (dyQuadraticDelta - currQuadraticDelta) / gradientDelta.y;
+
+        gradientOdds.x -= learning_rate.x * xDerivative;
+        gradientOdds.y -= learning_rate.y * yDerivative;
+        //printf("");
+    }
+    //printf("");
+
+    cout << "На сортировку лучше всего ложится: " << to_string(gradientOdds.x) << " * x * log(" << to_string(gradientOdds.y) << " * x)\n";
+}
+
+void MainCoorLinLayout::drawGradientOdds()
+{
+    for (int i = 0; i < maxArrLen; i++)
+    {
+        if (!app->getAppCondition()) break;
+        Vector newPoint = { i, logfnc(gradientOdds.x, gradientOdds.y, i) };
+
+        topWnd.addPoint(newPoint, gradientLogColor, 0, suggestedOddsIndex, false);
+    }
+    invalidateButton();
+}
+
+double MainCoorLinLayout::countQuadraticDelta(double k, double b)
+{
+    double answer = 0;
+    for (int currLen = 1; currLen < maxArrLen; currLen++)
+    {       
+        double fncRes = logfnc(k, b, currLen);
+        double delta = (fncRes - topWnd.getPoint(currLen, sortDataIndex).y);
+        answer += fabs(delta);
+    }
+    return answer;
+}
+
+double MainCoorLinLayout::logfnc(double k, double b, double x)
+{
+    return k * x * log(b * x);
 }
 
 void MainCoorLinLayout::randomArrFill(int* arr, int len)
--- MainCoorLinLayout.h
+++ MainCoorLinLayout.h
@@ -1,19 +1,35 @@
 #pragma once
 #include <LinearLayout.cpp>
-#include <CoordinatSystemWindow.cpp>
+#include <MultiLayCoordinatSystemWindow.cpp>
 
 struct MainCoorLinLayout : LinearLayout
 {
-    const int maxArrLen = 10000;
-    CoordinatSystemWindow topWnd;
-    CoordinatSystemWindow bottomWnd;
-    const int updateFrequency = 1000;
+    const int maxArrLen = 5000;
+    MultiLayCoordinatSystemWindow topWnd;
+    MultiLayCoordinatSystemWindow bottomWnd;
+    size_t sortDataIndex = 0;
+    size_t logIndex = 1;
+    size_t suggestedOddsIndex = 2;
+    COLORREF sortDataColor = C_BLUE;
+    COLORREF logColor = C_LIGHTRED;
+    COLORREF gradientLogColor = C_LIGHTGREEN;
+    
+    Vector gradientOdds = { 1, 2 };// k = x, b = y
+
+
+    bool isActiveComputingPart = true;
+    const int updateFrequency = 5000;
     UINT_PTR activeTimer = 0;
 
     MainCoorLinLayout(AbstractAppData* _app);
 
     void startComputations();
     void randomArrFill(int* arr, int len);
+
+    void gradientDescent();
+    void drawGradientOdds();
+    double countQuadraticDelta(double k, double b);
+    double logfnc(double k, double b, double x);//k * log(bx)
 
     virtual int onSize(Vector _managerSize, Rect _newRect) override;
     virtual int onTimer(UINT_PTR timerName) override;
