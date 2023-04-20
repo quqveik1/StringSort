e7b795e51ed4bbb837fce012217dbf90f598c569
Alex Bogomol <quqveik.1@gmail.com>
2023-04-20 23:12:10 +0300

    Trouble with russian

--- StringSort.cpp
+++ StringSort.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cctype>
 
 using namespace std;
 void readText(const string& path, string* originalfile);
@@ -15,22 +16,25 @@ void textSort(string** lines, int len, int (*cmp)(const void* str1, const void* 
 void printLines(string** str, int len);
 int startToBackCmp(const void* str1, const void* str2);
 int backToStartCmp(const void* str1, const void* str2);
-void saveText(string** str, int len, string path);
+void saveText(string** str, int len, const char* path);
+void saveText(string** str, int len, ofstream& stream);
 void workWithText();
-
-
-
-
+int getNextAlnumPos(const string& strs, int startIndex, int maxSize, int incrementDelta);
+void save3TextsOriginalFnc(const char* path, 
+                           string** startToBackLines, int startToBackLinesLen, 
+                           string** backToStartLines, int backToStartLinesLen,
+                           const string& fullText);
 
 
 int main()
 {
     //drawQuickSortResults();
+    workWithText();
 }
 
 void workWithText()
 {
-    string path = "test.txt";
+    string path = "RusOnegin.txt";
 
 
     string fulltext;
@@ -49,6 +53,7 @@ void workWithText()
     copyOriginalTextForSort(textLines, backToStartLines, stramount);
     textSort(backToStartLines, stramount, backToStartCmp);
 
+    /*
 
     cout << "Start to back sorted text:\n";
     printLines(startToBackLines, stramount);
@@ -58,13 +63,49 @@ void workWithText()
 
     saveText(startToBackLines, stramount, "startToBackLines.txt");
     saveText(backToStartLines, stramount, "backToStartText.txt");
+    */
+
+    save3TextsOriginalFnc("3Text.txt", startToBackLines, stramount, backToStartLines, stramount, fulltext);
 
     delete[] textLines;
     delete[] startToBackLines;
     delete[] backToStartLines;
 }
 
-void saveText(string** str, int len, string path)
+void save3TextsOriginalFnc(const char* path, 
+                           string** startToBackLines, int startToBackLinesLen, 
+                           string** backToStartLines, int backToStartLinesLen, 
+                           const string& fullText)
+{
+    ofstream stream(path);
+    if (stream.is_open())
+    {
+        stream << "Start to back sorted text:\n";
+        saveText(startToBackLines, startToBackLinesLen, stream);
+
+        stream << "\n-------------------------------------------------------------------------------------";
+        stream << "\nBack to start sorted text:\n";
+        saveText(backToStartLines, backToStartLinesLen, stream);
+
+        stream << "\n-------------------------------------------------------------------------------------";
+        stream << "\nOriginalText:\n";
+        stream << fullText;
+    }
+    stream.close();
+}
+
+void saveText(string** str, int len, ofstream& stream)
+{
+    if (stream.is_open())
+    {
+        for (int i = 0; i < len; i++)
+        {
+            stream << i << ": \"" << *(str[i]) << "\"\n";
+        }
+    }
+}
+
+void saveText(string** str, int len, const char* path)
 {
     ofstream stream(path);
 
@@ -78,7 +119,7 @@ void saveText(string** str, int len, string path)
     stream.close();
 }
 
-void printLines(string** str, int len)
+void printLines(const string** str, int len)
 {
     for (int i = 0; i < len; i++)
     {
@@ -99,32 +140,35 @@ void copyOriginalTextForSort(string* textLines, string** startToBackLines, int l
 
 int backToStartCmp(const void* str1, const void* str2)
 {
-
     string* _str1 = *((string**)str1);
     string* _str2 = *((string**)str2);
     int len1 = (int)_str1->length();
     int len2 = (int)_str2->length();
     int lenDelta = len2 - len1;
 
-    
-
     int minLen = min(len1, len2);
+    int index1 = len1 - 1;
+    int index2 = len2 - 1;
     for (int i = 1; i <= minLen; i++)
     {
-        int delta = (*_str1)[len1 - i] - (*_str2)[len2 - i];
+        index1 = getNextAlnumPos(*_str1, index1, len1, -1);
+        index2 = getNextAlnumPos(*_str2, index2, len2, -1);
+
+        if (index1 == -1 || index2 == -1) break;
+        int delta = (*_str1)[index1] - (*_str2)[index2];
+
+        index1--;
+        index2--;
         if (delta != 0)
         {
             return delta;
         }
     }
 
-
     if (lenDelta != 0)
     {
         return lenDelta;
     }
-
-    
 
     return 0;
 }
@@ -138,13 +182,23 @@ int startToBackCmp(const void* str1, const void* str2)
     int lenDelta = len2 - len1;
 
     int minLen = min(len1, len2);
+    int index1 = 0;
+    int index2 = 0;
     for (int i = 0; i < minLen; i++)
     {
-        int delta = (*_str1)[i] - (*_str2)[i];
+        index1 = getNextAlnumPos(*_str1, index1, len1, 1);
+        index2 = getNextAlnumPos(*_str2, index2, len2, 1);
+
+        if (index1 == -1 || index2 == -1) break;
+        int delta = (*_str1)[index1] - (*_str2)[index2];
+
+        index1++;
+        index2++;
+
         if (delta != 0)
         {
             return delta;
-        }
+        } 
     }
 
     if (lenDelta != 0)
@@ -155,7 +209,20 @@ int startToBackCmp(const void* str1, const void* str2)
     return 0;
 }
 
+int getNextAlnumPos(const string& strs, int startIndex, int maxSize, int incrementDelta)
+{
+    int answer = -1;
 
+    for (int i = startIndex; i < maxSize && i >= 0; i += incrementDelta)
+    {
+        if (isalnum(strs[i]))
+        {
+            return i;
+        }
+    }
+
+    return answer;
+}
 
 void textSort(string** lines, int len, int (*cmp)(const void* str1, const void* str2))
 {
